8b3ce187825f5c80da562616f16c3cc53272daf1
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-10-08 23:00:18 +0530

    fdsak

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,15 +1,16 @@
-const multer = require('multer')
+const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads')
+    cb(null, path.join(__dirname, 'uploads')); // Use path.join to specify the uploads folder
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
   }
-})
+});
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
-module.exports = upload ;
+module.exports = upload;
